9f21b402316905fd7681b6d84088230e60e8d919
Divya <divya@Darshans-MacBook-Air.local>
2024-03-14 13:07:51 -0700

    initial commit
     Initial commit

---
+++ CombineTutorial/CombineTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTutorialApp.swift
+//  CombineTutorial
+//
+//  Created by Divya on 2024-03-14.
+//
+
+import SwiftUI
+
+@main
+struct CombineTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineTutorial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CombineTutorial
+//
+//  Created by Divya on 2024-03-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
